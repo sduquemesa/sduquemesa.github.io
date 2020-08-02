72d7b6ca44b18101aee9777941a44f6c3c2691bc
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2020-08-02 11:57:20 -0500

    formated burger icon and menu fontsize

--- scripts/app.js
+++ scripts/app.js
@@ -15,7 +15,7 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7  + 0.25}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/20  + 0.25}s`;
             }
         });
 
@@ -34,7 +34,7 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7  + 0.25}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/20  + 0.25}s`;
             }
         });
 
